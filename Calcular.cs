4203827fcf9cffc674add55c1ab3212d9b344457
Jonathan <34173007+Joe5561@users.noreply.github.com>
2023-03-20 20:26:40 -0300

    Initial commit

---
+++ Calcular.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraPOO
+{
+    internal class Calcular  {
+
+        public double Somar(double n1 , double n2)
+        {
+            return  n1 + n2;
+        }
+
+        public double Subtrair(double n1 , double n2)
+        {
+            return  n1 - n2;
+        }
+
+        public double Divid(double n1 , double n2)
+        {
+            return  n1 / n2;
+        }
+
+        public double Mult(double n1 , double n2)
+        {
+            return  n1 * n2;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using CalculadoraPOO;
+
+
+Console.WriteLine("Digite o primeiro valor");
+double n1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o segundo valor");
+double n2 = double.Parse(Console.ReadLine());
+
+Calcular calcular = new Calcular();
+
+Console.WriteLine("Soma: " + calcular.Somar (n1, n2));
+Console.WriteLine("Subtração: " + calcular.Subtrair(n1, n2));
+Console.WriteLine("Multiplicar: " + calcular.Mult(n1, n2));
+Console.WriteLine("Dividir: " + calcular.Divid(n1, n2));
+
+Console.ReadLine();
+
+
+
